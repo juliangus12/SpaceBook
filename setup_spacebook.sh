b1a3c7bfe709d9a4c23a4a7369c9874351541b94
juliangus12 <143595370+juliangus12@users.noreply.github.com>
2023-10-22 20:18:34 -0400

    Create setup_spacebook.sh

---
+++ setup_spacebook.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Change ownership permissions of all files and directories to 777
+chmod -R 777 .
+
+# Install dependencies for the client
+cd client
+yarn add react-scripts
+
+# Install dependencies for the server
+cd ../server
+npm install express
+npm install
+
+# Start the server in the background
+npm start &
+
+# Change back to the client directory and start the client
+cd ../client
+yarn start
